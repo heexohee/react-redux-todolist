539a5b9a0efdeda38111c2c91c3416552842c8aa
heexohee <rsoy2918@naver.com>
2023-07-10 00:25:50 +0900

    주석 추가

---
+++ src/redux/modules/todos.js
@@ -0,0 +1,105 @@
+// Action value
+const ADD_TODO = "ADD_TODO";
+const GET_TODO_BY_ID = "GET_TODO_BY_ID";
+const DELETE_TODO = "DELETE_TODO";
+const TOGGLE_STATUS_TODO = "TOGGLE_STATUS_TODO";
+
+// Action Creator
+// Todo를 추가하는 action creator
+// 액션 생성함수는, 액션을 만드는 함수입니다. 
+// 단순히 파라미터를 받아와서 액션 객체 형태로 만들어주죠.
+
+export const addTodo = (payload) => {
+  return {
+    type: ADD_TODO,
+    payload,
+  };
+};
+
+// Todo를 지우는 action creator
+export const deleteTodo = (payload) => {
+  return {
+    type: DELETE_TODO,
+    payload,
+  };
+};
+
+// Todo를 isDone를 변경하는 action creator
+export const toggleStatusTodo = (payload) => {
+  return {
+    type: TOGGLE_STATUS_TODO,
+    payload,
+  };
+};
+
+// 상세 페이지에서 특정 Todo만 조회하는 action creator
+export const getTodoByID = (payload) => {
+  return {
+    type: GET_TODO_BY_ID,
+    payload,
+  };
+};
+
+// initial state
+const initialState = {
+  todos: [
+    {
+      id: "1",
+      title: "리액트",
+      body: "리액트를 배워봅시다",
+      isDone: false,
+    },
+  ],
+  todo: {
+    id: "0",
+    title: "",
+    body: "",
+    isDone: false,
+  },
+};
+
+// 리듀서는 변화를 일으키는 함수.
+// 리듀서는 두가지의 파라미터를 받아옴.
+// 리듀서는, 현재의 상태와, 전달 받은 액션을 참고하여 새로운 상태를 만들어서 반환
+const todos = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [...state.todos, action.payload],
+      };
+
+    case DELETE_TODO:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+
+    case TOGGLE_STATUS_TODO:
+      return {
+        ...state,
+        todos: state.todos.map((todo) => {
+          if (todo.id === action.payload) {
+            return {
+              ...todo,
+              isDone: !todo.isDone,
+            };
+          } else {
+            return todo;
+          }
+        }),
+      };
+
+    case GET_TODO_BY_ID:
+      return {
+        ...state,
+        todo: state.todos.find((todo) => {
+          return todo.id === action.payload;
+        }),
+      };
+    default:
+      return state;
+  }
+};
+
+export default todos;
